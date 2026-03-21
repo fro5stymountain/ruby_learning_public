b07f2c24d5666e6f34201d303ac68a0723e1534d
Jake Carter <jbcarter93@hotmail.com>
2026-03-21 21:02:23 +1000

    Filtering pattern

---
+++ kata/common_codewars_patterns/filtering-pattern.rb
@@ -0,0 +1,27 @@
+# Filtering Pattern
+# Used when the kata says:
+# remove
+# find only
+# return items that match condition
+# Ruby tool = select
+
+# Example
+def even_numbers(arr)
+  arr.select { |num| num.even? }
+end
+
+# input [1,2,3,4,5]
+# output => [2,4]
+
+# Loop version
+def l_even_numbers(arr)
+  result = []
+
+  arr.each do |num|
+    if num.even?
+      result << num
+    end
+  end
+
+  result
+end
