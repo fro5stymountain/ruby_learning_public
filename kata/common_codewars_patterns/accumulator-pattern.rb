d5ff83203002610e7f6728f6aef69bf1a0e1d109
Jake Carter <jbcarter93@hotmail.com>
2026-03-21 19:25:25 +1000

    accumulator pattern

---
+++ kata/common_codewars_patterns/accumulator-pattern.rb
@@ -0,0 +1,35 @@
+#1️ Accumulator Pattern (very common)
+
+#Used when you need to build a result step-by-step.
+
+#  Examples:
+
+#  sum numbers
+#build a string
+#collect items into a new array
+
+# Structure
+#result = starting_value
+
+# loop do
+#   result = result + something
+# end
+#
+# return result
+
+# Example: Sum numbers
+
+def sum_arrary(arr)
+  total = 0
+
+  arr.each do |num|
+    total += num
+  end
+
+  total
+end
+
+# Mental model
+# Start with nothing
+# Add things one at a time
+# Return final result
