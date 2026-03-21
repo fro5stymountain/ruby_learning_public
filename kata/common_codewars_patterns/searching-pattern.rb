19bbb43d08da7633bf54aaa4e67daa7174598df4
Jake Carter <156742859+fro5stymountain@users.noreply.github.com>
2026-03-21 21:18:45 +1000

    Jake/5 codewars patterns (#6)
    * accumulator pattern
    
    * transformation pattern with map and .each do loop
    
    * fix file name convention
    
    * Filtering pattern
    
    * Searching pattern)
    
    * Counter pattern

---
+++ kata/common_codewars_patterns/searching-pattern.rb
@@ -0,0 +1,27 @@
+#Searching Pattern
+#
+# Used when you must find something specific.
+#
+# Examples:
+#
+# smallest number
+# first match
+# highest score
+# Example: find smallest number
+
+def smallest(arr)
+  smallest = arr[0]
+
+  arr.each do |num|
+    if num < smallest
+      smallest = num
+    end
+  end
+
+  smallest
+end
+
+# Mental idea:
+# Assume first value is smallest
+# Compare everything else
+# Update when smaller
