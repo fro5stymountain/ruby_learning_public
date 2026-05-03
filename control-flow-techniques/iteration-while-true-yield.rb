ccb79236d52a9ced0a19f1b5137bda0076c23d86
Jake Carter <jbcarter93@hotmail.com>
2026-05-03 14:19:18 +1000

    Refactor to encapsulate staet in an object instead of global variables

--- control-flow-techniques/iteration-while-true-yield.rb
+++ control-flow-techniques/iteration-while-true-yield.rb
@@ -1,17 +1,39 @@
-print "Select loop type : 1 or 2 : "
-$input = gets.chomp.to_s
+class LoopSelector
+  def initialize(input)
+    @input = input
+  end
 
-def my_loop
-  while $input == "1"
-    yield
+  def my_loop(&block)
+    while @input == "1"
+      yield
+    end
+  end
+
+  # shorter version
+  def my_loop_s(&block)
+    yield while @input == "2"
   end
 end
 
-# shorter version
-def my_loop_s
-  yield while $input == "2"
+
+print "Select loop type : 1 or 2 : "
+input = gets.chomp.to_s
+selector = LoopSelector.new(input)
+
+case input
+when "1"
+  selector.my_loop { puts " Loop 1 chosen, this will print forever! Use ctrl+ c to stop it" }
+
+when "2"
+  selector.my_loop_s { puts "Loop 2 chosen, this will print forever! Use ctrl+ c to stop it" }
+
+else
+  puts "Error: Invalid input, please select either 1 or 2"
 end
 
-my_loop { puts " Loop 1 chosen, this will print forever! Use ctrl+ c to stop it" }
 
-my_loop_s { puts "Loop 2 chosen, this will print forever! Use ctrl+ c to stop it" }
+
+
+
+
+
