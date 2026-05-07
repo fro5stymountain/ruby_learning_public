e4e8ab5290423487597e735dab1dfd3079717017
Jake Carter <jbcarter93@hotmail.com>
2026-05-07 21:30:00 +1000

    namespace exception

--- control-flow-techniques/ensure-clause.rb
+++ control-flow-techniques/ensure-clause.rb
@@ -1,12 +1,16 @@
+class InvalidLineError < StandardError
+end
 def line_from_file(filename, substring)
   fh = File.open(filename)
-  begin
-    line = fh.gets
-    raise ArgumentError unless line.include?(substring)
-  rescue ArgumentError
-    puts "Invalid line"
-    raise
-  ensure fh.close
-  end
+  line = fh.gets
+  raise InvalidLineError unless line.include?(substring)
   return line
+
+rescue InvalidLineError
+  puts "Invalid line"
+  raise
+ensure
+  fh.close
 end
+
+line_from_file(ARGV[0], ARGV[1])
